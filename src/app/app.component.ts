a992c9ef1f3e63339d7ae7e137c6a40a5a8441c6
Newton Duarte <newton_@hotmail.com>
2022-05-20 09:31:16 -0300

    feat: add todo list app method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Todo } from 'src/models/todo.model';
+import { LocalstorageService } from './services/localstorage.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-todo';
+  todos: Todo[] = [];
+  title = 'Lista de Tarefas';
+  text = '';
+  form!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private localStorageService: LocalstorageService) {
+    this.form = this.formBuilder.group({
+      // title: ['', Validators.required] another way to build the form
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    });
+
+    this.loadAllTasksFromStorage();
+  }
+
+  add() {
+    const { title } = this.form.value;
+    const id = this.todos.length + 1;
+
+    this.todos.push(new Todo(id, title, false));
+    this.form.reset();
+
+    this.saveAllTasksOnStorage();
+  }
+
+  markAsDone(todo: Todo) {
+    todo.done = !todo.done;
+
+    this.saveAllTasksOnStorage();
+  }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+      this.saveAllTasksOnStorage();
+    }
+  }
+
+  saveAllTasksOnStorage() {
+    this.localStorageService.save('todos:newton', JSON.stringify(this.todos));
+  }
+
+  loadAllTasksFromStorage() {
+    const savedTodos = this.localStorageService.get('todos:newton');
+
+    if (!savedTodos) return;
+
+    this.todos = JSON.parse(savedTodos);
+  }
 }
